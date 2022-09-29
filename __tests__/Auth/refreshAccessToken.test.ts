6288bb7c46c7d2ea81b3e06ebda94a49be4c9fe6
KylianGERMAIN <63394149+KylianGERMAIN@users.noreply.github.com>
2022-09-29 17:20:39 +0200

    [ADD] test reset password and refresh token

---
+++ __tests__/Auth/refreshAccessToken.test.ts
@@ -0,0 +1,39 @@
+import request from "supertest";
+import app from "../../app";
+
+describe("REFRESH ACCESS TOKEN", () => {
+  test("refresh with refresh token simply", (done) => {
+    request(app)
+      .get(`/auth/refreshaccesstoken`)
+      .set("Authorization", "Bearer " + process.env.VALIDE_LOGIN_TOKEN)
+      .end((err: any, res: any) => {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(200);
+        expect(res._body.tokenType).toMatch("Bearer");
+        done();
+      });
+  });
+
+  test("refresh with nothing", (done) => {
+    request(app)
+      .get(`/auth/refreshaccesstoken`)
+      .end((err: any, res: any) => {
+        if (err) return done(err);
+        expect(res._body.statusCode).toEqual(404);
+        expect(res._body.message).toMatch("404 jwt not found");
+        done();
+      });
+  });
+
+  test("refresh with bad refresh token", (done) => {
+    request(app)
+      .get(`/auth/refreshaccesstoken`)
+      .set("Authorization", "Bearer " + process.env.EXPIRED_LOGIN_TOKEN)
+      .end((err: any, res: any) => {
+        if (err) return done(err);
+        expect(res._body.statusCode).toEqual(401);
+        expect(res._body.message).toMatch("401 jwt expired");
+        done();
+      });
+  });
+});
---
+++ __tests__/Auth/resetPassword.test.ts
@@ -0,0 +1,71 @@
+import request from "supertest";
+import app from "../../app";
+
+describe("RESET PASSWORD", () => {
+  test("reset password simply", (done) => {
+    request(app)
+      .post(`/auth/resetpassword`)
+      .set("Authorization", "Bearer " + process.env.VALIDE_LOGIN_TOKEN)
+      .send({
+        password: "jadore_krakow",
+      })
+      .end((err: any, res: any) => {
+        if (err) return done(err);
+        request(app)
+          .post(`/auth/resetpassword`)
+          .set("Authorization", "Bearer " + process.env.VALIDE_LOGIN_TOKEN)
+          .send({
+            password: process.env.PASSWORD_LOGIN_TEST,
+          })
+          .end((err: any, res: any) => {
+            if (err) return done(err);
+            expect(res._body.statusCode).toEqual(200);
+            expect(res._body.message).toMatch("OK");
+            done();
+          });
+        expect(res._body.statusCode).toEqual(200);
+        expect(res._body.message).toMatch("OK");
+        done();
+      });
+  });
+
+  test("reset password with nothing", (done) => {
+    request(app)
+      .post(`/auth/resetpassword`)
+      .end((err: any, res: any) => {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(500);
+        expect(res._body.errors[0].msg).toMatch("Password is missing");
+        done();
+      });
+  });
+
+  test("reset password with no header", (done) => {
+    request(app)
+      .post(`/auth/resetpassword`)
+      .send({
+        password: process.env.PASSWORD_LOGIN_TEST,
+      })
+      .end((err: any, res: any) => {
+        if (err) return done(err);
+        expect(res._body.statusCode).toEqual(404);
+        expect(res._body.message).toMatch("404 jwt not found");
+        done();
+      });
+  });
+
+  test("reset password with expired token", (done) => {
+    request(app)
+      .post(`/auth/resetpassword`)
+      .set("Authorization", "Bearer " + process.env.EXPIRED_LOGIN_TOKEN)
+      .send({
+        password: process.env.PASSWORD_LOGIN_TEST,
+      })
+      .end((err: any, res: any) => {
+        if (err) return done(err);
+        expect(res._body.statusCode).toEqual(401);
+        expect(res._body.message).toMatch("401 jwt expired");
+        done();
+      });
+  });
+});
