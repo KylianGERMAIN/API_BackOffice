bc3744f89c4709364517ee3315f520872c581c86
KylianGERMAIN <63394149+KylianGERMAIN@users.noreply.github.com>
2023-03-28 11:13:12 +0200

    [FIX] sql request and tabulation

--- Function/Utils/database.ts
+++ Function/Utils/database.ts
@@ -1,14 +1,14 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.USER_DB,
-  host: process.env.HOST_DB,
-  database: process.env.NAME_DB,
-  password: process.env.PASSWORD_DB,
-  port: process.env.PORT_DB,
+    user: process.env.USER_DB,
+    host: process.env.HOST_DB,
+    database: process.env.NAME_DB,
+    password: process.env.PASSWORD_DB,
+    port: process.env.PORT_DB,
 });
 
-export { pool }
+export { pool };
--- Middlewares/Auth/register.ts
+++ Middlewares/Auth/register.ts
@@ -1,11 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import {
-  responseEmailAlreadyExist,
-  responseErrorWhileGeneratingToken,
-  responseErrorWhileHashingToken,
-  responseErrorSearchingAccount,
-  responseErrorAddingAccount,
+    responseEmailAlreadyExist,
+    responseErrorWhileGeneratingToken,
+    responseErrorWhileHashingToken,
+    responseErrorSearchingAccount,
+    responseErrorAddingAccount,
 } from "../../Function/Response/responseUser";
 import { pool } from "../../Function/Utils/database";
 import { generateRefreshAcccesTokens } from "../../Function/Utils/generateToken";
@@ -14,60 +14,64 @@ import { User } from "../../Interfaces/User";
 const bcrypt = require("bcrypt");
 
 function addUserToDb(user: User, res: Response) {
-  pool.query(
-    `INSERT INTO public.users("id", "email", "password", "date") VALUES('${user.id}', '${user.email}', '${user.password}', '${user.date}')`,
-    (error: any) => {
-      if (error) {
-        responseErrorAddingAccount(res);
-        return false;
-      }
-      return true;
-    }
-  );
-  return true;
+    pool.query(
+        `INSERT INTO public.users("id", "email", "password", "date") VALUES('${user.id}', '${user.email}', '${user.password}', '${user.date}')`,
+        (error: any) => {
+            if (error) {
+                responseErrorAddingAccount(res);
+                return false;
+            }
+            return true;
+        }
+    );
+    return true;
 }
 
 function hashPassword(user: User, res: Response) {
-  const saltRounds = 15;
+    const saltRounds = 15;
 
-  bcrypt.genSalt(saltRounds, function (err: any, salt: any) {
-    if (err) {
-      responseErrorWhileGeneratingToken(res);
-      return;
-    }
-    bcrypt.hash(user.password, salt, async function (err: any, hash: any) {
-      if (err) {
-        responseErrorWhileHashingToken(res);
-        return;
-      }
-      user.password = hash;
-      if ((await addUserToDb(user, res)) === true) {
-        res.status(StatusCodes.CREATED).json(generateRefreshAcccesTokens(user));
-      }
+    bcrypt.genSalt(saltRounds, function (err: any, salt: any) {
+        if (err) {
+            responseErrorWhileGeneratingToken(res);
+            return;
+        }
+        bcrypt.hash(user.password, salt, async function (err: any, hash: any) {
+            if (err) {
+                responseErrorWhileHashingToken(res);
+                return;
+            }
+            user.password = hash;
+            if ((await addUserToDb(user, res)) === true) {
+                res.status(StatusCodes.CREATED).json(
+                    generateRefreshAcccesTokens(user)
+                );
+            }
+        });
     });
-  });
 }
 
 export async function register(req: Request, res: Response) {
-  var uuid = require("uuid");
-  const user: User = {
-    id: uuid.v4(),
-    email: req.body.email,
-    password: req.body.password,
-    date: Date.now().toString(),
-  };
-  await pool.query(
-    `SELECT * FROM Public.users WHERE email like '${user.email}'`,
-    (error: any, results: { rows: any }) => {
-      if (error) {
-        responseErrorSearchingAccount(res);
-      } else {
-        if (rowIsVoid(results.rows) === true) {
-          hashPassword(user, res);
-        } else {
-          responseEmailAlreadyExist(res);
+    var uuid = require("uuid");
+    const user: User = {
+        id: uuid.v4(),
+        email: req.body.email,
+        password: req.body.password,
+        date: Date.now().toString(),
+    };
+
+    await pool.query(
+        `SELECT * FROM public.users WHERE email like '${user.email}'`,
+        (error: any, results: { rows: any }) => {
+            if (error) {
+                console.log(error);
+                responseErrorSearchingAccount(res);
+            } else {
+                if (rowIsVoid(results.rows) === true) {
+                    hashPassword(user, res);
+                } else {
+                    responseEmailAlreadyExist(res);
+                }
+            }
         }
-      }
-    }
-  );
+    );
 }
